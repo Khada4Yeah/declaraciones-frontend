70cfaaa870ea6f72f9877be463d38678881e58fc
pjespinoza <pespinoza9494@utm.edu.ec>
2025-12-26 13:34:55 -0500

    arreglo url https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://declaraciones.khadayeah-server.online/api/'
+    apiUrl: 'https://declaraciones.khadayeah-server.online/api/'
 };

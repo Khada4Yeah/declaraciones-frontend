2861fbe7f7de5e01b46e88ec73f7a0161f39c034
pjespinoza <pespinoza9494@utm.edu.ec>
2025-12-26 13:27:22 -0500

    Agrego nueva url de API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://khadayeah-server.online/declaraciones/api/'
+    apiUrl: 'http://declaraciones.khadayeah-server.online/api/'
 };

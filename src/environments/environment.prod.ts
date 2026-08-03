a94408fb2eb0844b611bc22f1ca5d9458b3dcb41
pjespinoza <pespinoza9494@utm.edu.ec>
2026-08-03 11:07:42 -0500

    Cambio url endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://declaraciones.khadayeah-server.online/api/'
+    apiUrl: 'https://declaraciones.khadayeah-server.fit/api/'
 };
